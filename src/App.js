751539c1f8017b056a95a878c99e8b545103772a
itc-s21009 <s21009@std.it-college.ac.jp>
2022-06-28 21:40:31 +0900

    トップページを作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NewsList from "./news/NewsList";
 import Header from "./common/Header";
+import Top from "./index/Top";
 
 const App = () =>
     <>
         <Header />
         <BrowserRouter>
             <Routes>
+                <Route exact path={"/"} element={<Top />}/>
                 <Route exact path={"/news"} element={<NewsList />}/>
             </Routes>
         </BrowserRouter>
---
+++ src/index/Top.js
@@ -0,0 +1,18 @@
+import mezamashikun from '../images/mezamashi.jpg'
+
+const Top = () =>
+    <>
+        <h1 className={"text-center"}>TOP</h1>
+        <div className={"position-absolute h-75 w-100 m-0 d-flex align-items-center justify-content-center"}>
+            <div className="card text-center">
+                <img src={mezamashikun} height={300}
+                     className="card-img-top" alt={"めざましくん"}/>
+                <div className="card-body">
+                    <h5 className="card-title">ニュースを見る</h5>
+                    <a href="/news" className="btn btn-primary stretched-link">LET'S GO</a>
+                </div>
+            </div>
+        </div>
+    </>
+
+export default Top
