e61c7ef07a9435d74d23b245d2be9422d414f41e
itc-s21009 <s21009@std.it-college.ac.jp>
2022-06-28 22:58:24 +0900

    インデント

--- src/common/NotFound.js
+++ src/common/NotFound.js
@@ -1,7 +1,7 @@
 import not_found from '../images/not_found.jpg'
 
 const NotFound = () =>
-     <>
+    <>
         <h1 className={"text-center"}>Not Found</h1>
         <div className={"position-absolute h-75 w-100 m-0 d-flex align-items-center justify-content-center"}>
             <img src={not_found} height={"100%"} alt={"not found"}/>
