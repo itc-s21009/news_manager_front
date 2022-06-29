7f4434b7be64af0feb57107abe4f6b9328ff7d2a
itc-s21009 <s21009@std.it-college.ac.jp>
2022-06-29 10:24:49 +0900

    タイトルの見た目を変えた

--- src/news/NewsSingle.js
+++ src/news/NewsSingle.js
@@ -17,11 +17,14 @@ const NewsSingle = () => {
     return (
         <>
             <div className={"news p-2"}>
-                <div className={"d-flex align-items-end"}>
+                <div className={"title d-flex align-items-end"}>
                     <h1>{data["title"]}</h1>
                     <h5>by {data["post_by"]}</h5>
                 </div>
-                <h6 className={"date"}>{translateDate(data["publish_at"])}</h6>
+                <div className={"title d-flex"}>
+                    <h6>{data["category"]}</h6>
+                    <h6 className={"date"}>{translateDate(data["publish_at"])}</h6>
+                </div>
                 <hr/>
                 <div className={"body bg-light m-auto p-3"}>
                     {String(data["body"]).split("\n").map((line, index) =>
