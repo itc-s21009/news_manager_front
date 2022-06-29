c0232b04d33527465c4c494b7cf4fdecb521faad
itc-s21009 <s21009@std.it-college.ac.jp>
2022-06-29 16:09:23 +0900

    ログイン画面を日本語にした

--- src/login/Login.js
+++ src/login/Login.js
@@ -9,18 +9,18 @@ const Login = () =>
             <h1 className={"mt-3 text-center"}>ログイン</h1>
             <div className={"login p-3 mt-3 m-auto"}>
                 <div className={"mb-3 row p-3"}>
-                    <label htmlFor={"username"}>Username</label>
+                    <label htmlFor={"username"}>ユーザーID</label>
                     <input type={"text"} id={"username"}/>
                 </div>
                 <div className={"mb-3 row p-3"}>
-                    <label htmlFor={"password"}>Password</label>
+                    <label htmlFor={"password"}>パスワード</label>
                     <input type={"password"} id={"password"}/>
                 </div>
                 <button onClick={() => {
                     const username = document.getElementById("username").value
                     const password = document.getElementById("password").value
                     PostLogin(username,password)
-                }} className={"btn btn-primary w-100"}>Login
+                }} className={"btn btn-primary w-100"}>ログイン
                 </button>
             </div>
         </>
