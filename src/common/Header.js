9be6562615af8fbffb5a20e0b9a608d3a674e6f7
itc-s21009 <s21009@std.it-college.ac.jp>
2022-06-30 10:28:54 +0900

    微調整

--- src/common/Header.js
+++ src/common/Header.js
@@ -6,7 +6,7 @@ const Header = () =>
             <div className={"container"}>
                 <div className={"row"}>
                     <div className={"col-6"}>
-                        <a href={"/"} className={'text-decoration-none text-white'}><h1>サーバーサイド Kotlin 課題</h1></a>
+                        <a href={"/"} className={'text-decoration-none text-white'}><h2>サーバーサイド Kotlin 課題</h2></a>
                     </div>
                     <div className={"col-3 offset-3 menu d-flex"}>
                         <a href={"/news"} className={"text-decoration-none text-white"}><h5>ニュース</h5></a>
